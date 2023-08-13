d50deb6050704733b6fb61c3a7ccc2e14eccf9c4
Robert Smith <67526754+therls@users.noreply.github.com>
2023-08-12 20:17:14 -0400

    Added spring and next.js

---
+++ dvd-server/src/main/java/com/rsmith/dvdserver/DvdServerApplication.java
@@ -0,0 +1,13 @@
+package com.rsmith.dvdserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdServerApplication.class, args);
+	}
+
+}
